5bdcd7bb21a545ce8dc7a225e337b7932eac93bc
aki <mark@aur.ll.to>
2020-05-12 11:14:14 +0900

    Mish
    処理時間1.1倍、Accuracy 2ポイント減と、いいとこ無し…。

--- train1000.py
+++ train1000.py
@@ -14,6 +14,7 @@ import numpy as np
 import scipy
 import sklearn.metrics
 import tensorflow as tf
+import tensorflow_addons as tfa
 
 logger = logging.getLogger(__name__)
 
@@ -240,7 +241,7 @@ def create_network(input_shape, num_classes):
         tf.keras.layers.BatchNormalization,
         gamma_regularizer=tf.keras.regularizers.l2(1e-4),
     )
-    act = functools.partial(tf.keras.layers.Activation, "relu")
+    act = functools.partial(tf.keras.layers.Activation, tfa.activations.mish)
 
     def blocks(filters, count, down=True):
         def layers(x):
